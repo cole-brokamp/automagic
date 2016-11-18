e3bed8ca1f1260f355e84a5ad195723c13e0401e
Cole Brokamp <cole.brokamp@gmail.com>
2016-11-18 00:34:33 -0500

    clean up namespaces

--- R/deps_file.R
+++ R/deps_file.R
@@ -3,16 +3,15 @@
 #' @param directory directory containing R code to parse
 #'
 #' @export
-#'
-#' This function parses R code and then queries the R package library to
+#' @details This function parses R code and then queries the R package library to
 #' determine the exact source and version of each package to install. Currently,
 #' only CRAN and GitHub packages are supported using a version number and Sha1
 #' key, respectively. Install packages from the .dependencies file using
 #' \code{\link[automagic]{install_deps_file}}
+#' @seealso \code{\link{automagic}}
 make_deps_file <- function(directory=getwd()) {
   packrat::opts$snapshot.recommended.packages(TRUE, persist = FALSE)
-  packrat::.snapshotImpl(project = directory,snapshot.sources=FALSE,verbose=FALSE,prompt=FALSE) %>%
-    suppressMessages()
+  packrat::.snapshotImpl(project = directory,snapshot.sources=FALSE,verbose=FALSE,prompt=FALSE)
   file.rename(from=file.path(directory, 'packrat', 'packrat.lock'),
               to=file.path(directory,'.dependencies'))
   unlink(file.path(directory,'packrat'),recursive=TRUE,force=TRUE)
@@ -23,22 +22,22 @@ make_deps_file <- function(directory=getwd()) {
 #' @param directory directory containing .dependencies file
 #'
 #' @export
-#'
-#' Installs packages from GitHub and CRAN based on Sha1 key and version number
+#' @details Installs packages from GitHub and CRAN based on Sha1 key and version number
 #' respectively, as defined in a .dependencies file created by
 #' \code{\link[automagic]{make_deps_file}}
+#' #' @seealso \code{\link{automagic}}
 install_deps_file <- function(directory=getwd()) {
   set_repo()
 
   deps_file <- file.path(directory,'.dependencies')
   stopifnot(file.exists(deps_file))
-  deps <- read.dcf(deps_file) %>%
-    as.data.frame(stringsAsFactors = FALSE)
+  deps <- read.dcf(deps_file)
+  deps <- as.data.frame(deps,stringsAsFactors = FALSE)
 
-  gh_deps <- deps %>% filter(Source == 'github') %>%
-    mutate(install_call = paste0(GithubUsername,'/',GithubRepo,'@',Hash))
-  cran_deps <- deps %>% filter(Source == 'CRAN') %>%
-    select(-contains('Git'))
+  gh_deps <-  dplyr::filter(deps,deps$Source == 'github')
+  gh_deps$install_call <- with(gh_deps,paste0(GithubUsername,'/',GithubRepo,'@',Hash))
+  cran_deps <- dplyr::filter(deps,deps$Source == 'CRAN')
+  cran_deps <- dplyr::select(cran_deps,-dplyr::contains('Git'))
 
   # install CRAN package given version number
   purrr::walk2(cran_deps$Package,cran_deps$Version,devtools::install_version,type='source')
