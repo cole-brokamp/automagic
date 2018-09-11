8c2b8c901e61278d0c43c615ffd9607f47effa6e
Cole Brokamp <cole.brokamp@gmail.com>
2018-09-11 12:39:53 -0400

    added failing tests for #7

--- tests/testthat/samples/Simple.R
+++ tests/testthat/samples/Simple.R
@@ -4,3 +4,5 @@ require(methods)
 require('splines')
 tools::file_ext("simple.R")
 MASS:::abbey
+xfun::pkg_attach(c('ggplot2', 'dplyr', 'shiny'))
+pacman::p_load('sf')
--- tests/testthat/test-parse_packages.R
+++ tests/testthat/test-parse_packages.R
@@ -1,7 +1,15 @@
 context("parse_packages")
+
 test_that("Identifies packages correctly in .R files",{
   expect_equal(
-    parse_packages("samples/simple.R"),
-    c("stats","boot","methods","splines","tools", "MASS")
+    parse_packages("samples/Simple.R"),
+    c("stats","boot","methods","splines","tools", "MASS", "xfun", "ggplot2", "dplyr", "shiny", "pacman", "sf")
+  )
+})
+
+test_that("Identifies packages correctly in .Rmd files",{
+  expect_equal(
+    parse_packages("samples/Simple.Rmd"),
+    c("stats","boot","methods","splines","tools", "MASS", "xfun", "ggplot2", "dplyr", "shiny", "pacman", "sf")
   )
 })
